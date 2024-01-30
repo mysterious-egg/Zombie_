c5262ba1b06abf0f2f1cd3feeb3206666120a149
Manvesh Thakur <manveshthakur0102@gmail.com>
2024-01-31 01:44:10 +0530

    Added

---
+++ score.js
@@ -0,0 +1,17 @@
+// Iteration 8: Making scoreboard functional
+
+var urlParams = new URLSearchParams(window.location.search);
+console.log(urlParams);
+var score = urlParams.get("score");
+
+var scoreBoard = document.getElementById("score-board");
+
+var playAgainButton = document.getElementById("play-again-button");
+
+scoreBoard.innerHTML = score;
+
+
+
+playAgainButton.onclick = () => {
+  location.href = "./game.html";
+};
